7607cb74a7614904503a27b46f7f68a4b510ac50
Paweł Brzozowski <pawel@brzoza.net>
2024-03-11 15:47:51 +0100

    Create contacts.js

---
+++ contacts.js
@@ -0,0 +1,6 @@
+// Zaimportuj moduły fs i path do pracy z systemem plików.
+// Utwórz zmienną contactsPath i zapisz w niej ścieżkę do pliku contacts.json.
+// Do utworzenia ścieżki wykorzystaj metody modułu path.
+// Dodaj funkcję do pracy ze zbiorem kontaktów.
+// W funcjach wykorzystaj moduł fs oraz jego metody readFile() i writeFile().
+// Zrób eksport utworzonych funkcji przez module.exports.
